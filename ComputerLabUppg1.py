c5e4d605a7b62a34a26ffaaf1434647ac89beb26
PiIVkY <johanolofalm@gmail.com>
2025-04-03 18:25:20 +0200

    Meckat rätt de sista små gremlinsarna

--- ComputerLabUppg1.py
+++ ComputerLabUppg1.py
@@ -13,15 +13,16 @@ import calfem.vis_mpl as cfv
 
 import numpy as np
 
-num_elements = 3
+num_elements = 20
 
 x_0 = 2
 x_1 = 8
+Q=100
 
-ex = np.zeros((num_elements+1, 2))
+ex = np.zeros((num_elements, 2))
 
-for i in range(num_elements+1):
-    ex[i] = [x_0 + (x_1-x_0)*i/(num_elements+1), x_0 + (x_1-x_0)*(i+1)/(num_elements+1)]
+for i in range(num_elements):
+    ex[i] = [x_0 + (x_1-x_0)*i/(num_elements), x_0 + (x_1-x_0)*(i+1)/(num_elements)]
 
 edof = np.zeros((num_elements,2), dtype=int)
 
@@ -39,12 +40,23 @@ for i in range(num_elements):
     kei = cfc.bar1e(ex[i, :], [E, A])
     cfc.assem(edof[i, :], K, kei)
 
-F = np.zeros((num_elements+1, 1))
-F[num_elements] = -15
+Fb = np.zeros((num_elements+1, 1))
+Fb[num_elements] = -15*A
+
 bc_dof = np.array([1])
 bc_val = np.array([0])
 
-print(K)
+Fl = np.zeros((num_elements+1, 1))
+for i in range(len(Fl)):
+    Fl[i] = Q*(x_1-x_0)/num_elements
+Fl[0] = Q*(x_1-x_0)/(2*num_elements)
+Fl[-1] = Q*(x_1-x_0)/(2*num_elements)
+
+F = Fb + Fl
+
+#print(K)
+#print(F)
+print(ex)
 a, r = cfc.solveq(K, F, bc_dof , bc_val)
 
 print("Temp:")
